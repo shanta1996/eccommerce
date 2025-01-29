3ee0c8698e545aad109975e51cd44172a594b9e2
shanta1996 <shantachaudhary490@gmail.com>
2025-01-29 09:16:09 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './components/Home'
+import About from './components/About'
+import Products from './components/Products'
+import Contact from './components/Contact'
+import SingleProduct from './components/SingleProduct'
+import CartPage from './components/CartPage'
 
 const router=createBrowserRouter([
   {
-    
     path:'/',
     element:<Home/>
-    
-  }
+  },
+  {
+    path:'/about',
+    element:<About/>
+  },
+  {
+    path:'/products',
+    element:<Products/>
+  },
+  {
+    path:'/contact',
+    element:<Contact/>
+  },
+  {
+    path:'/singleProduct',
+    element:<SingleProduct/>
+  },
+  {
+    path:'/cart',
+    element:<CartPage/>
+  },
 ])
 const App = () => {
   return (
